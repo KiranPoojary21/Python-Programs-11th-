aab34bc9224cd392d2e4eecd12b4aea1196ca683
KiranPoojary21 <kiranpoojary2109@gmail.com>
2024-09-29 08:17:35 +0530

    Add files via upload

---
+++ To Calculate Simple Intrest.py
@@ -0,0 +1,9 @@
+# This Program Was Made To Calculate Simple Intrest Of Any Value
+
+p = float(input("Enter Value Of Principle"))
+t = int(input("Enter Value Of Time"))
+r = float(input("Enter Value Of Rate"))
+ 
+Simple_Intrest = p*t*r/100
+
+print("The Calculated Value Of Simple Intrest Is",Simple_Intrest)
